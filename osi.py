3fa5dee48aee51d3f48c0bc493ffd9ae3348cb6f
Rodri <81118866+lVoidi@users.noreply.github.com>
2023-11-24 19:30:03 -0600

    Added domain existence check

--- osi.py
+++ osi.py
@@ -7,8 +7,11 @@ red = colorama.Fore.RED
 green = colorama.Fore.GREEN
 magenta = colorama.Fore.MAGENTA
 yellow = colorama.Fore.YELLOW
+reset = colorama.Fore.RESET
 
-print(magenta + """
+print(
+    magenta
+    + """
  ▄█▀▀▀█▄█           ▄██                    ▀███                                     ██                    
 ▄██    ▀█            ██                      ██                                                           
 ▀███▄   ▀███  ▀███   ██▄████▄           ▄█▀▀███   ▄██▀██▄▀████████▄█████▄  ▄█▀██▄ ▀███ ▀████████▄  ▄██▀███
@@ -18,43 +21,45 @@ print(magenta + """
 █▀█████▀  ▀████▀███▄ █▀█████▀          ▀████▀███▄ ▀█████▀▄████  ████  ████▄████▀██▄████▄████  ████▄██████▀
 
                                         By: Little.Kid
-""")
+"""
+)
 
-print(yellow + "ingresa de un dominio.")
-lw = input(">> ")
-if lw == "":
-    print("ingresa algo XDDDDDDDDDDDDDDDDD")
-    sys.exit()
-else:
-    pass
-def ss():
-    with open("sub.txt","r") as osi:
-        for cosa in osi:
-            enlace = f"http://{cosa.strip()}.{lw}"
-            enlace2 = f"www.{cosa.strip()}.{lw}"
-            print("")
-            try:
-                rr = requests.get(enlace)
-                if rr.status_code == 200:
-                    print(green + "Dominio:", enlace2)
-                elif rr.status_code == 404:
-                    print(red + "Dominio:", enlace2)
-                else:
-                    pass
-            except requests.exceptions.ConnectionError:
-                print(red + f"Dominio: {enlace2}")
-            try:
-                pp = socket.gethostbyname(enlace2)
-                print(green + f"IP: {pp}")
-            except socket.gaierror:
-                pass
-            try:
-                tt = requests.get(enlace)
-                yy = tt.headers
-                if "Server" in yy and "cloudflare" in yy["Server"].lower():
-                    print(green + "CloudFlare:",green + "True")
-                else:
-                    print(green +"CloudFlare:", red + "False")
-            except requests.exceptions.ConnectionError:
-                pass
-ss()
+
+def domain_exists(domain: str):
+    try:
+        request = requests.get(domain, timeout=10)
+    except ConnectionError:
+        print(f"[{red}ERROR{reset}] Domain does NOT exist")
+        return False
+    else:
+        return True
+
+
+domain = input(f"{yellow} Domain >>")
+
+while not domain_exists(domain):
+    print(f"[{red}ERROR]{reset}] You have to write a valid domain")
+    domain = input(f"{yellow} Domain >>")
+
+
+with open("sub.txt", "r") as osi:
+    for con in osi:
+        link_1 = f"http://{con.strip()}.{domain}"
+        link_2 = f"www.{con.strip()}.{domain}"
+        try:
+            rr = requests.get(link_1)
+        except requests.exceptions.ConnectionError:
+            print(red + f"Domain: {link_2}")
+            continue
+        try:
+            pp = socket.gethostbyname(link_2)
+            print(green + f"IP: {pp}")
+        except socket.gaierror:
+            print(f"[{red}ERROR{reset}] No address assosiated: {link_2}")
+            continue
+        req = requests.get(link_1)
+        headers = req.headers
+        if "Server" in headers.keys() and "cloudflare" in headers["Server"].lower():
+            print(green + "CloudFlare: True")
+        else:
+            print(green + "CloudFlare:", red + "False")
